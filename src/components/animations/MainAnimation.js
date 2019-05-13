9a91b8718f37c12845a1c8b4c464d652d1e23c47
Anna Costalonga <Adsidera@users.noreply.github.com>
2019-05-13 10:35:13 +0200

    changed to one single svg

--- src/components/animations/MainAnimation.js
+++ src/components/animations/MainAnimation.js
@@ -1,11 +1,9 @@
 import React from "react"
-import illustration from "../../images/disegno.svg"
-import caption from "../../images/meme_caption.svg"
+import illustration from "../../images/meme.svg"
 
 const MainAnimation = () => (
   <div className="meme-div">
     <img className="slide-in-top inline" src={illustration} alt="maria" />
-    <img className="slide-in-top-2 inline" src={caption} alt="meme" />
   </div>
 )
 
