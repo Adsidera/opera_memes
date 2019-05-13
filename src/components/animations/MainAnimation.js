912c0240956065d47f5fd2f858e54b37c3e1e7e3
Adsidera <annamaria.costalonga@gmail.com>
2019-05-13 22:51:50 +0200

    used just one svg file

--- src/components/animations/MainAnimation.js
+++ src/components/animations/MainAnimation.js
@@ -1,5 +1,5 @@
 import React from "react"
-import illustration from "../../images/disegno.svg"
+import illustration from "../../images/meme_caption.svg"
 
 const MainAnimation = () => (
   <div className="meme-div">
